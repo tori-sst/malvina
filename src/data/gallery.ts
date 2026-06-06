9d364705076ee2b5e0f4c21b4f214313f6fcbec5
Vik <viktoriastermer@gmail.com>
2026-06-06 14:07:20 +0500

    сборка модулей

---
+++ src/data/gallery.ts
@@ -0,0 +1,8 @@
+export const galleryImages = [
+  '/img/img1.jpg',
+  '/img/img2.jpg',
+  '/img/img3.jpg',
+  '/img/profil-1.jpg',
+  '/img/profil-2.jpg',
+  '/img/profil-3.jpg',
+];
---
+++ src/data/services.ts
@@ -0,0 +1,39 @@
+export interface ServiceItem {
+  title: string;
+  includes: string[];
+  duration: string;
+  price: string;
+}
+
+export const servicesData: ServiceItem[] = [
+  {
+    title: 'Детский час',
+    includes: ['аренда лошади', 'помощь тренера', 'площадка', 'экипировка'],
+    duration: '1 час',
+    price: '1500 руб'
+  },
+  {
+    title: 'Детский час',
+    includes: ['аренда лошади', 'помощь тренера', 'площадка', 'экипировка'],
+    duration: '1 час',
+    price: '1500 руб'
+  },
+  {
+    title: 'Детский час',
+    includes: ['аренда лошади', 'помощь тренера', 'площадка', 'экипировка'],
+    duration: '1 час',
+    price: '1500 руб'
+  },
+  {
+    title: 'Детский час',
+    includes: ['аренда лошади', 'помощь тренера', 'площадка', 'экипировка'],
+    duration: '1 час',
+    price: '1500 руб'
+  },
+  {
+    title: 'Детский час',
+    includes: ['аренда лошади', 'помощь тренера', 'площадка', 'экипировка'],
+    duration: '1 час',
+    price: '1500 руб'
+  }
+];
