5badca3a9c145b751ba629e0c230e64e5411fabb
Vik <viktoriastermer@gmail.com>
2026-06-06 18:31:21 +0500

    оптимизация изображений

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -1,8 +1,8 @@
 export const galleryImages = [
-  '/malvina/img/img1.jpg',
-  '/malvina/img/img2.jpg',
-  '/malvina/img/img3.jpg',
-  '/malvina/img/profil-1.jpg',
-  '/malvina/img/profil-2.jpg',
-  '/malvina/img/profil-3.jpg',
+  '/malvina/img/img1.webp',
+  '/malvina/img/img2.webp',
+  '/malvina/img/img3.webp',
+  '/malvina/img/profil-1.webp',
+  '/malvina/img/profil-2.webp',
+  '/malvina/img/profil-3.webp',
 ];
